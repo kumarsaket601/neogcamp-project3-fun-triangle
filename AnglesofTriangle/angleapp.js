5d94984e7a84c40b0b3f10bb33a73624e5c71861
Saket kumar <kumarsaket601@gmail.com>
2021-08-11 17:02:00 +0530

    update in js

--- AnglesofTriangle/angleapp.js
+++ AnglesofTriangle/angleapp.js
@@ -3,20 +3,20 @@ var secondAngle = document.querySelector("#second-angle");
 var thirdAngle = document.querySelector("#third-angle");
 var submitBtn = document.querySelector("#submit-button");
 var display = document.querySelector(".output");
-var log = console.log;
+
 
 function calculateAngleHandler(){
     
     
-    var sumOfAngle = Number(firstAngle.value) + Number(secondAngle.value) + Number(thirdAngle.value);
+    var sumOfAngle =  Number(firstAngle.value) + Number(secondAngle.value) + Number(thirdAngle.value);
     
-     if(sumOfAngle === 180){
+    if(sumOfAngle === 180){
          
         display.innerText = "Using this you can make an triangle";
     }
     else{
         display.innerText = "Oops! sorry triangle can not be made";
     }
-};
+}
 
 submitBtn.addEventListener('click', calculateAngleHandler)
