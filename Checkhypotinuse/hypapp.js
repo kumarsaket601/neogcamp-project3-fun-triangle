14b566c2c705844d9e2276a94d88c9808e83748d
Saket kumar <kumarsaket601@gmail.com>
2021-09-17 09:42:58 +0530

    Update hypapp.js

--- Checkhypotinuse/hypapp.js
+++ Checkhypotinuse/hypapp.js
@@ -1,30 +1,35 @@
 var firstSide = document.querySelector("#first-side");
 var secondSide = document.querySelector("#second-side");
-var submitBtn =  document.querySelector("#submit-button");
+var submitBtn = document.querySelector("#submit-button");
 var display = document.querySelector(".output");
 
 
 
-function calculateHypo(first,second){
+function calculateHypo(first, second) {
 
-     var hypotonuse = Math.sqrt(Number(first*first) + Number(second*second));
-     
+     var hypotonuse = Math.sqrt(Number(first * first) + Number(second * second));
+
      return hypotonuse;
 }
-function clickHandler(){
 
-    if(Number(firstSide.value) >= 0 && Number(secondSide.value)){
+function clickHandler() {
 
-         var thirdSide = calculateHypo(Number(firstSide.value),Number(secondSide.value))
-     
-     display.innerText = "Hypotonuse is " + thirdSide + " cm";
-    }
-    
-     else{
-          display.innerText = "Please Provide positive value";
+     if (Number(firstSide.value) != '' && Number(secondSide.value) != '') {
+          if (Number(firstSide.value) > 0 && Number(secondSide.value) > 0) {
+
+               var thirdSide = calculateHypo(Number(firstSide.value), Number(secondSide.value))
+
+               display.innerText = "Hypotonuse is " + thirdSide + " cm";
+          } else {
+               display.innerText = "Please provide positive value";
+          }
+     } else {
+          display.innerText = "Provide value in the input.";
      }
-     
-     
+
+
+
+
 };
 
-submitBtn.addEventListener('click',clickHandler)
+submitBtn.addEventListener('click', clickHandler)
