d1c22bc8668cb8c27c33571a3dff173530b9cb96
Saket kumar <kumarsaket601@gmail.com>
2021-09-17 09:46:16 +0530

    Update areaapp.js

--- calculatearea/areaapp.js
+++ calculatearea/areaapp.js
@@ -3,30 +3,36 @@ var heightOfTriangle = document.querySelector("#height");
 var outputDiv = document.querySelector(".display");
 var submitBtn = document.querySelector("#submit-btn");
 
-function calculateArea(baseOfTriangle,heightOfTriangle){
+function calculateArea(baseOfTriangle, heightOfTriangle) {
 
-     var areaTraingle = (baseOfTriangle*heightOfTriangle)/2;
-     
+     var areaTraingle = (baseOfTriangle * heightOfTriangle) / 2;
+
      return areaTraingle;
-     
+
 }
 
-function areaOfTriangle(){
-     
-     if( Number(baseOfTriangle.value) >= 0 && Number(heightOfTriangle.value) >= 0) {
+function areaOfTriangle() {
 
-           var area = calculateArea(Number(baseOfTriangle.value),Number(heightOfTriangle.value));
+     if (Number(baseOfTriangle.value) != '' && Number(heightOfTriangle.value) != '') {
+          if (Number(baseOfTriangle.value) > 0 && Number(heightOfTriangle.value) > 0) {
 
-    
+               var area = calculateArea(Number(baseOfTriangle.value), Number(heightOfTriangle.value));
 
-      outputDiv.innerText = "Area of Triangle is " + area + " cm square";
 
-     }else{
 
-          outputDiv.innerText = "Please Provide with the positive  value";
+               outputDiv.innerText = "Area of Triangle is " + area + " cm square";
+
+          } else {
+
+               outputDiv.innerText = "Please provide with the positive value.";
+          }
+     } else {
+          outputDiv.innerText = "Provide value in the input.";
      }
 
-   
+
+
+
 }
 
 submitBtn.addEventListener("click", areaOfTriangle)
